3654f03c18770b6f772e48ecf97656fffb2596fc
Cristian Mendieta <cjmm0811@gmail.com>
2024-03-17 20:13:17 +0100

    Trabajos en ciencia de datos

---
+++ R/4_1_Primer_script.R
@@ -0,0 +1,28 @@
+a <- 5
+b <- 2.0
+c <- '5'
+
+# Comentario de una l�nea
+
+"
+Comentario de varias l�neas
+de
+c�digo"
+
+# Información sobre docstring en R: https://cran.r-project.org/web/packages/docstring/vignettes/docstring_intro.html
+
+z <- 3
+
+# NOTA: Opci�n de comentar y descomentar varias l�neas de c�digo
+# 1. Selecci�n de las l�neas
+# 2. Sobre las l�neas seleccionadas: CNTRL + SHIFT + C
+
+# Prueba a comentar/descomentar el siguiente c�digo
+
+funcion.suma <- function(valor_a, valor_b){
+
+
+  suma <- valor_a + valor_b
+
+  return (suma)
+}
